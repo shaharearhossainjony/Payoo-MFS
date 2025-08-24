a7809d6795862b96800a3805f2ff4ae23880af9f
shaharearhossainjony <shaharearhossainjony@gmail.com>
2025-08-24 23:05:49 +0600

    pin updated

--- script.js
+++ script.js
@@ -1,5 +1,23 @@
 // login button functionality 
 document.getElementById('loginButton').addEventListener('click', function(e){
     e.preventDefault();
-    console.log('btn clicked');
+    const mobileNumber = 1771420235;
+    const fourDigitPin = 1133;
+    const mobileNumberValue= document.getElementById('mobile-number').value;
+    const mobileNumberConverted= parseInt(mobileNumberValue);
+
+    const pinNumberValue = document.getElementById('pin-number').value;
+    const pinNumberConverted = parseInt(pinNumberValue);
+
+    console.log(mobileNumberConverted, pinNumberConverted );
+
+    if( mobileNumberConverted === mobileNumber &&  pinNumberConverted === fourDigitPin){
+        window.location.href="./home.html" ;
+    }
+    else{
+        alert("Invalid Credentials");
+
+    }
+
+
 })
